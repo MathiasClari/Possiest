c0364aeca7c1f6d6b3f29b0432f2cd46197f0c73
Mathias Clari Drenik <66589714+MathiasClari@users.noreply.github.com>
2023-01-30 02:39:29 +0100

    Add files via upload

--- app/head.js
+++ app/head.js
@@ -2,6 +2,22 @@ const Head = () => (
   <>
     <title>Possiest Alpha</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta property="og:title" content="Possiest Alpha" />
+      <meta property="og:title" content="Possiest | play.possiest.com" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://possiest.com" />
+      <meta property="og:image" content="https://cosmetica.cc/page/splash/Eyezah-Tuikat.jpg" />
+      <meta property="og:site_name" content="Possiest" />
+      <meta property="og:locale" content="en-us"/>
+          <meta property="og:description" content="Possiest is a dynamic Minecraft server with popular game modes like BedWars, Survival, and WildLands a MMORPG mode. Join now and experience a diverse and exciting gaming experience!"/>
+              <meta name="twitter:card" content="summary_large_image"/>
+                  <meta property="twitter:domain" content="possiest.com"/>
+                      <meta property="twitter:url" content="https://possiest.com"/>
+                          <meta name="twitter:title" content="Possiest - play.possiest.com"/>
+                              <meta name="twitter:description" content="Possiest is a dynamic Minecraft server with popular game modes like BedWars, Survival, and WildLands a MMORPG mode. Join now and experience a diverse and exciting gaming experience!"/>
+                                  <meta name="twitter:image" content="https://i.imgur.com/hg7FBVF.png"/>
+                                      <meta name="theme-color" content="#32C3EA"/>
+
     <link rel="icon" href="/possiest.ico" />
   </>
 );
